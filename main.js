1461760c2d85c0e33473c498fbdb02dc2bfab590
nbuylding <nbuylding@live.ca>
2023-01-06 19:28:27 -0500

    number counter project

---
+++ main.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+const btns = document.querySelectorAll('.btn');
+const value = document.querySelector('#value');
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')) {
+            count--;
+        }
+
+        else if(styles.contains('increase')) {
+            count++;
+        }
+
+        else{
+            count = 0;
+        }
+
+        value.textContent = count;
+    });
+});
